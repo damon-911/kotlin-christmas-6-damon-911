238b190b0640d02e107558f31dd081b82ac81881
Minjun Kim <damon911@naver.com>
2023-11-13 11:56:19 +0900

    refactor: OutView에서는 로직 계산을 하지 않고 출력 기능만 담당하게끔 코드 리팩토링

--- src/main/kotlin/christmas/controller/EventPlannerController.kt
+++ src/main/kotlin/christmas/controller/EventPlannerController.kt
@@ -1,5 +1,6 @@
 package christmas.controller
 
+import christmas.model.RestaurantMenu
 import christmas.view.InputView
 import christmas.view.OutputView
 
@@ -29,6 +30,43 @@ class EventPlannerController {
     }
 
     private fun showEventBenefits() {
-        outputView.printEventBenefits(date, menu)
+        outputView.printEventBenefits(date)
+        showOrderedMenu()
+        val totalOrderAmount = showTotalOrderAmountBeforeDiscount()
+        showGiftMenu(totalOrderAmount)
+        val totalBenefitsAmount = showBenefitsDetail()
+    }
+
+    private fun showOrderedMenu() {
+        outputView.printOrderedMenu(menu)
+    }
+
+    private fun showTotalOrderAmountBeforeDiscount(): Int {
+        var total = 0
+        menu.forEach {
+            total += RestaurantMenu.getMenuPrice(it.first) * it.second
+        }
+        outputView.printTotalOrderAmountBeforeDiscount(total)
+        return total
+    }
+
+    private fun showGiftMenu(totalOrderAmount: Int) {
+        outputView.printGiftMenu(totalOrderAmount)
+    }
+
+    private fun showBenefitsDetail(): Int {
+        outputView.printBenefitsDetail()
+        var totalDiscount = 0
+        totalDiscount += checkChristmasDiscount()
+        return totalDiscount
+    }
+
+    private fun checkChristmasDiscount(): Int {
+        if (date in 1..25) {
+            val discount = -(1000 + (date - 1) * 100)
+            outputView.printChristmasDiscount(discount)
+            return discount
+        }
+        return 0
     }
 }
--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -1,6 +1,5 @@
 package christmas.view
 
-import christmas.model.RestaurantMenu
 import christmas.util.constant.Texts
 
 class OutputView {
@@ -17,51 +16,36 @@ class OutputView {
         println(Texts.MESSAGE_INPUT_MENU.text)
     }
 
-    fun printEventBenefits(date: Int, menu: List<Pair<String, Int>>) {
+    fun printEventBenefits(date: Int) {
         println("12월 ${date}일에 ${Texts.MESSAGE_EVENT_BENEFITS.text}")
-        printOrderedMenu(menu)
-        val flag = printTotalOrderAmountBeforeDiscount(menu)
-        printGiftMenu(flag)
-        printBenefitsDetail(date, menu)
     }
 
-    private fun printOrderedMenu(menu: List<Pair<String, Int>>) {
+    fun printOrderedMenu(menu: List<Pair<String, Int>>) {
         println("\n" + Texts.MESSAGE_ORDERED_MENU.text)
         menu.forEach {
             println("${it.first} ${it.second}개")
         }
     }
 
-    private fun printTotalOrderAmountBeforeDiscount(menu: List<Pair<String, Int>>): Boolean {
+    fun printTotalOrderAmountBeforeDiscount(totalAmount: Int) {
         println("\n" + Texts.MESSAGE_TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT.text)
-        var totalAmount = 0
-        menu.forEach {
-            totalAmount += RestaurantMenu.getMenuPrice(it.first) * it.second
-        }
         println(String.format("%,d원", totalAmount))
-        return totalAmount >= 120_000
     }
 
-    private fun printGiftMenu(flag: Boolean) {
+    fun printGiftMenu(totalOrderAmount: Int) {
         println("\n" + Texts.MESSAGE_GIFT_MENU.text)
-        if (flag) {
+        if (totalOrderAmount >= 120_000) {
             println(Texts.MESSAGE_GIFT.text)
             return
         }
         println(Texts.MESSAGE_NONE.text)
     }
 
-    private fun printBenefitsDetail(date: Int, menu: List<Pair<String, Int>>) {
+    fun printBenefitsDetail() {
         println("\n" + Texts.MESSAGE_BENEFITS_DETAIL.text)
-        var totalDiscount = 0
-        totalDiscount += checkChristmasDiscount(date)
     }
 
-    private fun checkChristmasDiscount(date: Int): Int {
-        val discount = -(1000 + (date - 1) * 100)
-        if (date in 1..25) {
-            println(Texts.MESSAGE_BENEFITS_CHRISTMAS.text + String.format("%,d원", discount))
-        }
-        return discount
+    fun printChristmasDiscount(discount: Int) {
+        println(Texts.MESSAGE_BENEFITS_CHRISTMAS.text + String.format("%,d원", discount))
     }
 }
